210e06e25a191592086905750befa263f82492b0
ubuntu <nizar.jalal98@gmail.com>
2017-02-19 13:52:41 +0200

    modify index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var html = fs.readFileSync(__dirname + 'index.html' , 'utf8');
 http.createServer(function(req,res){
    res.end("html");
- }).listen(process.env.PORT,function(){
+ }).listen(process.env.PORT || 8080,function(){
     console.log('Listening on 8080');
 });
