20f28327eca2b7e09bdcf95a1cdc914633e4203f
ubuntu <nizar.jalal98@gmail.com>
2017-02-19 13:55:23 +0200

    edit js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var html = fs.readFileSync(__dirname + 'index.html' , 'utf8');
+var html = fs.readFileSync(__dirname + '/index.html' , 'utf8');
 http.createServer(function(req,res){
    res.end("html");
  }).listen(process.env.PORT || 8080,function(){
