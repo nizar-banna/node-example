b7874196de19cce1e2841eb27cc6b20cabcf4c85
ubuntu <nizar.jalal98@gmail.com>
2017-02-19 16:15:16 +0200

    edit js to show html file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var html = fs.readFileSync(__dirname + '/index.html' , 'utf8');
 http.createServer(function(req,res){
-   res.end("html");
+   res.end(html);
  }).listen(process.env.PORT || 8080,function(){
     console.log('Listening on 8080');
 });
