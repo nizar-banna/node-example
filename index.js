9ce8757cbd2371d34aef200257a5c355ed520642
ubuntu <nizar.jalal98@gmail.com>
2017-02-19 13:46:57 +0200

    dd

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
-
+var fs = require('fs');
+var html = fs.readFileSync(__dirname + 'index.html' , 'utf8');
 http.createServer(function(req,res){
-   res.end("hello");
+   res.end("html");
  }).listen(process.env.PORT,function(){
     console.log('Listening on 8080');
 });
